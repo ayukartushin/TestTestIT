cdc95ff00af9b00c798d9f62b9305dda50c08c44
AY_Kartushin <akartyshin@tsconsulting.ru>
2021-08-02 23:37:57 +0300

    commit
    1

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,29 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestClass {
+
+    private WebDriver driver;
+
+    @After
+    public void Close(){
+        if (driver != null)
+            driver.quit();
+    }
+    
+    @Test
+    public void GoogleTest(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        driver.get("http://google.com/");
+        driver.findElement(By.name("q")).sendKeys("otus" + Keys.ENTER);
+        Assert.assertEquals("OTUS", driver.findElement(By.cssSelector("div.g h3")).getText());
+    }
+}
