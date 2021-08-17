41b289a4a69355b9fbaa5587401f48c7ba3a7947
AY_Kartushin <akartyshin@tsconsulting.ru>
2021-08-17 17:06:52 +0300

    1

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,6 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -8,10 +9,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.testit.annotations.Step;
+import ru.testit.annotations.Title;
+import ru.testit.annotations.WorkItemId;
 
 public class TestClass {
 
     private WebDriver driver;
+
+    @Before
+    public void Open(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        driver = new ChromeDriver(chromeOptions);
+    }
 
     @After
     public void Close(){
@@ -20,16 +32,22 @@ public class TestClass {
     }
     
     @Test
+    @WorkItemId("14")
+    @Title("АТ.Поиск Отуса в гугл")
     public void GoogleTest(){
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless");
-        driver = new ChromeDriver(chromeOptions);
-        //driver = new FirefoxDriver();
-
-
-        driver.get("http://google.com/");
-        driver.findElement(By.name("q")).sendKeys("otus" + Keys.ENTER);
+        Step1();
+        Step2();
         Assert.assertEquals("OTUS", driver.findElement(By.cssSelector("div.g h3")).getText());
+    }
+
+    @Step
+    @Title("Открыть гугл")
+    private void Step1(){
+        driver.get("http://google.com/");
+    }
+    @Step
+    @Title("Поиск")
+    private void Step2(){
+        driver.findElement(By.name("q")).sendKeys("otus" + Keys.ENTER);
     }
 }
